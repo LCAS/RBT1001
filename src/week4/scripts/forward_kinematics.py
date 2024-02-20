2e64a6cc874bdfa8d766973f8864af008ed05f52
francescodelduchetto <francescodelduchetto@gmail.com>
2024-02-20 10:52:20 +0000

    WIP week4

---
+++ src/week4/scripts/forward_kinematics.py
@@ -0,0 +1,25 @@
+from sympy import *
+from transformations import *
+
+
+def symbolic_FK():
+    """
+    Generates the symbolic homogeneous transformation matrix for the CR7iA robot (6DOF)
+    from the robot base to the end effector frame,
+    composing homogeneous rotations and translations.
+
+    Name the angles q1,q2,q3,q4,q5,q6 and use appropriate names for translations based on
+    the robot datasheet.
+
+    Returns
+    -------
+    (4x4) sympy.Matrix: symbolic homogenous transformation matrix
+    """
+    # TODO: write the composition of rotations and traslations
+    T = 
+
+    return T
+
+def 
+
+if __name__ == "__main__":
