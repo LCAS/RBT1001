93d9a94997909ff846e2f6b2f3f66638be5328cf
francescodelduchetto <francescodelduchetto@gmail.com>
2024-02-22 12:38:27 +0000

    moveit dependencies

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -15,8 +15,6 @@ sudo chown -R lcas /workspaces
 source /opt/ros/lcas/install/setup.bash
 sudo apt update
 
-# instal moveit
-sudo apt install ros-humble-moveit
 
 rosdep --rosdistro=humble update 
 
@@ -31,7 +29,25 @@ rosdep --rosdistro=humble update
 # colcon build
 
 
+cd /home/lcas/ws/src
+git clone -b ros2 https://github.com/PickNikRobotics/bio_ik.git
+git clone -b humble https://github.com/ros-planning/moveit2_tutorials.git
+git clone -b humble https://github.com/ros-planning/moveit_task_constructor.git
+git clone -b humble https://github.com/ros-planning/moveit_resources.git
+git clone https://github.com/PickNikRobotics/ros2_robotiq_gripper.git
+git clone -b ros2 https://github.com/tylerjw/serial.git
+# vcs import < moveit2_tutorials/moveit2_tutorials.repos
+
+# instal moveit
+sudo apt install ros-humble-moveit -y
+# sudo apt install ros-humble-moveit-resources -y
+sudo apt install ros-humble-pick-ik -y
+# sudo apt install python3-pykdl
+
 cd /home/lcas/ws
+
+rosdep install -r --from-paths . --ignore-src --rosdistro $ROS_DISTRO -y
+
 colcon build 
 add_source_bashrc "/opt/ros/lcas/install/setup.bash"
 add_source_bashrc "/home/lcas/ws/install/setup.bash"
