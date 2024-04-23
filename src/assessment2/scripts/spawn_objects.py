c08a34e791e13ea22cca9597847dac8999576775
francescodelduchetto <francescodelduchetto@gmail.com>
2024-04-23 17:04:21 +0100

    WIP

--- src/assessment2/scripts/spawn_objects.py
+++ src/assessment2/scripts/spawn_objects.py
@@ -2,7 +2,8 @@ import sys
 import time
 import numpy as np
 import rclpy
-from gazebo_msgs.srv import SpawnEntity, DeleteEntity
+from gazebo_msgs.srv import SpawnEntity, DeleteEntity, GetModelList
+from geometry_msgs.msg import Pose
 
 from rclpy.node import Node
 
@@ -11,38 +12,106 @@ class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__('spawn_entity')
+
+        self.models_client = self.create_client(GetModelList, 'get_model_list')
+        while not self.models_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Get Models service not available, waiting again...')
+        self.models_req = GetModelList.Request()
+        self.get_logger().info('Get Models service available.')
+
         self.spawn_client = self.create_client(SpawnEntity, 'spawn_entity')
         while not self.spawn_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
-        self.spawn_req = SpawnEntity.Request()
+            self.get_logger().info('Spawn service not available, waiting again...')
+        self.get_logger().info('Spawn service available.')
 
         self.del_client = self.create_client(DeleteEntity, 'delete_entity')
         while not self.del_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
-        self.del_req = DeleteEntity.Request()
+            self.get_logger().info('Delete service not available, waiting again...')
+        self.get_logger().info('Delete service available.')
+
+        self._publishers = [
+            self.create_publisher(Pose, '/A_pose', 10),
+            self.create_publisher(Pose, '/B_pose', 10),
+            self.create_publisher(Pose, '/C_pose', 10)
+        ]
 
     def send_request(self):
 
         object_names = ["A", "B", "C"]
         object_colors = [
-            "1.0 0.1 0.1 1.0",
-            "0.0 1.0 0.0 1.0",
-            "0.0 0.0 1.0 1.0"
+            "1.0 0.0 0.2 1.0",
+            "0.1 0.6 0.1 1.0",
+            "0.0 0.3 1.0 1.0"
         ]
+        color_names = ["red", "green", "blue"]
 
-        for (name, color) in zip(object_names, object_colors):
 
-            self.del_req.name = name
-            self.future = self.del_client.call_async(self.del_req)
+        future = self.models_client.call_async(self.models_req)
 
-            time.sleep(1)
+        rclpy.spin_until_future_complete(self, future)
+        # while rclpy.ok() and not future.done():
+        print("{}".format(future.result()))
+            # time.sleep(0.5)
+        
+        existing_models = [m for m in future.result().model_names if m in object_names]
 
-            self.spawn_req.name = name
-            self.spawn_req.xml = "<?xml version=\"1.0\" ?><sdf version=\"1.5\"><model name=\"will_be_ignored\"><static>true</static><link name=\"link\"><visual name=\"visual\"><geometry><sphere><radius>0.04</radius></sphere></geometry><material><ambient rgba=\"{0:}\"/><diffuse rgba=\"{0:}\"/><specular rgba=\"0.2 0.2 0.2 1.0\"/><emissive>{0:}</emissive></material></visual></link></model></sdf>".format(color)
-            self.spawn_req.initial_pose.position.x = np.random.rand() * 1.1
-            self.spawn_req.initial_pose.position.y = np.random.rand() * 2. - 1.
-            self.spawn_req.initial_pose.position.z = 0.5 + (np.random.rand() * 0.5)
-            self.future = self.spawn_client.call_async(self.spawn_req)
+        for (name, color, color_name, publisher) in zip(object_names, object_colors, color_names, self._publishers):
+
+            # delete previous object
+            if name in existing_models:
+                del_req = DeleteEntity.Request()
+                del_req.name = name
+                future = self.del_client.call_async(del_req)
+                rclpy.spin_until_future_complete(self, future)
+                print("Deleted object {}".format(name))
+                # while rclpy.ok() and not future.done():
+                #     time.sleep(0.5)
+                time.sleep(2)
+
+            # create Pose object message
+            pose = Pose()
+            pose.position.x = 0.2 + np.random.rand() * 0.5
+            pose.position.y = np.random.rand() * 1.5 - 0.75
+            pose.position.z = 0.1 + (np.random.rand() * 1.5)
+
+            # create gazebo spawn request
+            spawn_req = SpawnEntity.Request()
+            spawn_req.name = name
+            spawn_req.xml = " \
+                <?xml version=\"1.0\" ?><sdf version=\"1.5\"> \
+                <model name=\"will_be_ignored\"><static>true</static> \
+                <link name=\"link\"> \
+                <visual name=\"visual\"> \
+                    <geometry> \
+                        <sphere><radius>0.04</radius></sphere> \
+                    </geometry> \
+                    <material> \
+                        <ambient>{0:}</ambient> \
+                        <diffuse>0.5 0.5 0.5 1</diffuse> \
+                        <specular>1 1 1 1</specular> \
+                        <emissive>{0:}</emissive> \
+                    </material> \
+                </visual> \
+                </link></model></sdf>".format(color)
+            spawn_req.initial_pose.position.x = pose.position.x
+            spawn_req.initial_pose.position.y = pose.position.y
+            spawn_req.initial_pose.position.z = pose.position.z
+
+            # spawn new object
+            future = self.spawn_client.call_async(spawn_req)
+            rclpy.spin_until_future_complete(self, future)
+
+            # publish on topic
+            publisher.publish(pose)
+            
+            self.get_logger().info("{}: {}, ({:.3f}, {:.3f}, {:.3f})".format(
+                name, 
+                color_name, 
+                pose.position.x,
+                pose.position.y,
+                pose.position.z
+            ))
+
 
 
 def main(args=None):
@@ -51,18 +120,21 @@ def main(args=None):
     minimal_client = MinimalClientAsync()
     minimal_client.send_request()
 
-    while rclpy.ok():
-        rclpy.spin_once(minimal_client)
-        if minimal_client.future.done():
-            try:
-                response = minimal_client.future.result()
-            except Exception as e:
-                minimal_client.get_logger().info(
-                    'Service call failed %r' % (e,))
-            else:
-                minimal_client.get_logger().info(
-                    'Test {}'.format(minimal_client.spawn_req.xml))
-            break
+    rclpy.spin_once(minimal_client)
+    rclpy.spin_once(minimal_client)
+
+    # while rclpy.ok():
+    # rclpy.spin_once(minimal_client)
+        # if minimal_client.future.done():
+        #     try:
+        #         response = minimal_client.future.result()
+        #     except Exception as e:
+        #         minimal_client.get_logger().info(
+        #             'Service call failed %r' % (e,))
+        #     else:
+        #         minimal_client.get_logger().info(
+        #             'Done')
+        #     break
 
     minimal_client.destroy_node()
     rclpy.shutdown()
