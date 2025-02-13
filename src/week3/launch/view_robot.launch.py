a6763a34fa70b4104ce14480e3c49ac4521459de
francescodelduchetto <francescodelduchetto@gmail.com>
2025-02-13 12:14:29 +0000

    6dof mecharm

--- src/week3/launch/view_robot.launch.py
+++ src/week3/launch/view_robot.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="rrbot.urdf.xacro",
+            default_value="simple_robot.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
