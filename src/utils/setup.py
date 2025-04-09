44f6dc62a2e3774f8b04dab39131d6366dfbc308
francescodelduchetto <francescodelduchetto@gmail.com>
2025-04-09 19:52:18 +0100

    fixing utils package

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -1,6 +1,4 @@
 from setuptools import find_packages, setup
-from os import path
-from glob import glob
 
 package_name = 'utils'
 
@@ -9,23 +7,20 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # (path.join('share', package_name, 'launch'), glob(path.join('launch', '*launch.[pxy][yma]*'))),
-        # (path.join('share', package_name, 'description', 'urdf'), glob(path.join('description', 'urdf', '*.xacro'))),
-        # (path.join('share', package_name, 'description', 'meshes', 'visual'), glob(path.join('description', 'meshes', 'visual', '*'))),
-        (path.join('share', package_name, 'scripts'), glob(path.join('scripts', '*.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Francesco Del Duchetto',
-    maintainer_email='fdelduchetto@lincoln.ac.uk',
-    description='RBT1001 utils package',
-    license='Apache-2.0',
+    maintainer='lcas',
+    maintainer_email='francescodelduchetto@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'simple_marker = scripts.simple_marker:main',
-            'target_marker = scripts.target_marker:main'
+            'simple_marker = utils.simple_marker:main'
         ],
     },
 )
